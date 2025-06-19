ef02ea152d8b6326ea57bcd6285b09d21cbffdbd
megario55 <megarajan147@gmail.com>
2025-06-19 11:41:26 +0530

    update server 19/6/25

--- Backend/server.js
+++ Backend/server.js
@@ -20,10 +20,10 @@ connectDB();
 
 // Middleware
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 // Routes
