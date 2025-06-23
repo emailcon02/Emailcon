aa2d78e73c1ecd9b1be160ed77f72ba5a72783f5
Abcd <renugajagadeesan@gmail.com>
2025-06-23 15:42:27 +0530

    upd logo 23/6/25

--- Backend/api/apiconfigfrontend.js
+++ Backend/api/apiconfigfrontend.js
@@ -1,8 +1,8 @@
 // src/apiConfig.js
 const apiconfigfrontend = {
-  // baseURL:"http://localhost:8000",
-    // baseURL:"http://localhost:3000",
+//   baseURL:"http://localhost:8000",
+    //baseURL:"http://localhost:3000",
     baseURL:"https://emailcon.in"
 };
 
-export default apiconfigfronten
+export default apiconfigfrontend;
