ce46c3687721e25559129ce9a820cdd6ad680387
megario55 <megarajan147@gmail.com>
2025-07-15 12:44:25 +0530

    upd home 15/7/25

--- Backend/models/Camhistory.js
+++ Backend/models/Camhistory.js
@@ -1,105 +1,96 @@
-import mongoose from "mongoose";
-
 const camhistorySchema = new mongoose.Schema({
-    campaignname: {
-        type: String,
-        required: true,
+  campaignname: {
+    type: String,
+    required: true,
+  },
+  groupname: {
+    type: String,
+    required: true,
+  },
+  totalcount: {
+    type: String,
+    required: true,
+  },
+  progress: {
+    type: String,
+    required: true,
+  },
+  aliasName: {
+    type: String,
+    required: true,
+  },
+  replyTo: {
+    type: String,
+    required: true,
+  },
+  sendcount: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  previewtext: {
+    type: String,
+    required: true,
+  },
+  failedcount: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  senddate: {
+    type: String,
+    required: true,
+  },
+  sentEmails: {
+    type: [String],
+  },
+  failedEmails: {
+    type: [String],
+  },
+  previewContent: {
+    type: Array,
+    required: true,
+  },
+  attachments: [
+    {
+      originalName: { type: String },
+      fileUrl: { type: String },
     },
-    groupname: {
-        type: String,
-        required: true,
+  ],
+  bgColor: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  scheduledTime: {
+    type: Date,
+  },
+  recipients: {
+    type: String,
+  },
+  groupId: {
+    type: String,
+  },
+  exceldata: [
+    {
+      Fname: { type: String },
+      Lname: { type: String },
+      Email: { type: String },
+      additionalFields: {
+        type: Map,
+        of: String,
+      },
     },
-    totalcount: {
-        type: String,
-        required: true,
-    },
-    progress: {
-        type: String,
-        required: true,
-    },
-    aliasName: {
-        type: String,
-        required: true,
-    },
-    replyTo: {
-        type: String,
-        required: true,
-    },
-    sendcount: {
-        type: String,
-        required: true,
-    },
-    subject: {
-        type: String,
-        required: true,
-    },
-    previewtext: {
-        type: String,
-        required: true,
-    },
-    failedcount: {
-        type: String,
-    },
-    status: {
-        type: String,
-        required: true,
-    },
-    senddate: {
-        type: String,
-        required: true,
-    },
-    sentEmails: {
-        type: [String],
-    },
-    failedEmails: {
-        type: [String],
-    },
-    previewContent: {
-        type: Array,
-        required: true,
-    },
-    attachments: [
-        {
-          originalName: { type: String},
-          fileUrl: { type: String},
-        }
-      ],
-    bgColor: {
-        type: String,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    scheduledTime: {
-        type: Date,
-    },
-    recipients: {
-        type: String,
-    },
-    groupId: {
-        type: String,
-    },
-    exceldata: [{
-        Fname: {
-            type: String,
-        },
-        Lname: {
-            type: String,
-        },
-        Email: {
-            type: String,
-        },
-        additionalFields: {
-            type: Map,
-            of: String,
-        },
-    }],
-},{
-    strict: false
-}, {
-    timestamps: true, // Automatically stores createdAt and updatedAt
+  ],
+},
+{
+  strict: false,
+  timestamps: true, 
 });
-
-const Camhistory = mongoose.model("Camhistory", camhistorySchema);
-export default Camhistory;
